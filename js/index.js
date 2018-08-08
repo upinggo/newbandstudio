b3ad976d8f3592c84dce78509d86372a78bbfcb8
apple <upingshen@gmail.com>
2018-08-08 10:31:39 +0800

    newband

---
+++ js/index.js
@@ -0,0 +1,18 @@
+// window.onload = function(){
+//     var w=window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+//     var h=window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+//     document.getElementById("containerbox").style.width = w+"px";
+//     document.getElementById("containerbox").style.height = h+"px";
+//     }
+//     window.onresize = function(){
+//         var w=window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+//         var h=window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+//         document.getElementById("containerbox").style.width = w+"px";
+//         document.getElementById("containerbox").style.height = h+"px";
+//         }
+window.onload=function(){
+   var c= document.querySelector("#sylppp");
+   var p=document.createElement("p");
+   p.innerHTML="fdhash"
+   c.appendChild(p)
+}

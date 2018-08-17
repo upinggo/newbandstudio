cacd48c65a6c333fdac7e000534a301153377427
apple <upingshen@gmail.com>
2018-08-17 19:02:16 +0800

    commit

--- js/index.js
+++ js/index.js
@@ -1,34 +1,61 @@
 // import {fetch} from 'whatwg-fetch'; ajax fetch方法！
 var common=new common();
 var url=common.staticurl+common.api.banner;
-    
-fetch(url,{
-    method: 'GET',
-    
-}).then(e=>e.json()).then(e=>console.log(e))
+var imgurl=[];
 
+
+window.onresize=function(){
+    var home=this.document.getElementById('home');
+    var homeheight=this.document.getElementById('home').offsetHeight;
+    var bodyheight=document.body.clientHeight;
+    home.style.top=(bodyheight-homeheight)/2+'px';
+    var container=document.getElementById('container');
+    
+    // container.style.background=
+}
 window.onload=function(){
-// swiper
-var mySwiper = new Swiper ('.swiper-container', {
-    autoplay:true,
-    loop: true,
-    
-    // 如果需要分页器
-    pagination: {
-      el: '.swiper-pagination',
-    },
-    
-    // 如果需要前进后退按钮
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    
-   
-  })
-  
-
-
+    fetch(url,{
+        method: 'GET',
+        
+    }).then(e=>e.json()).then(function(e){
+        console.log(e.slide_show)
+        for(var i=0;i<e.slide_show.length;i++){
+            imgurl.push(common.staticurl+e.slide_show[i].original_src)
+        }
+    }
+    ).then(
+        function(e){
+            var bannerimg=this.document.getElementById('bannerimg');
+            var html='';
+            for(var i=0;i<imgurl.length;i++){
+                html+='<div class="swiper-slide"><img src="'+imgurl[i]+'" alt=""></div>';
+                bannerimg.innerHTML=html;
+            }
+        }
+    ).then(function(e){
+        var home=this.document.getElementById('home');
+        var homeheight=this.document.getElementById('home').offsetHeight;
+        var bodyheight=document.body.clientHeight;
+        home.style.top=(bodyheight-homeheight)/2+'px';  
+    // swiper
+    var mySwiper = new Swiper ('.swiper-container', {
+        autoplay:true,
+        loop: true,
+        
+        // 如果需要分页器
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        
+        // 如果需要前进后退按钮
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        
+       
+      })
+    });
     var title=this.document.getElementsByClassName('title');
     var self=this;
     
@@ -59,9 +86,7 @@ var mySwiper = new Swiper ('.swiper-container', {
         
         
     }
-    document.addEventListener('mousewheel',function(e){
-        console.log(e)
-    },false)
+    
 }
 
 

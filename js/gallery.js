dfc3c7202b05a2c09cae489a19320e88e42df6f3
apple <upingshen@gmail.com>
2018-09-11 18:12:17 +0800

    done

--- js/gallery.js
+++ js/gallery.js
@@ -7,13 +7,39 @@ $(document).ready(function(){
     var num=data.photo.length;
     for(var i=0;i<num;i++){
         var img=document.createElement("img");
-        img.setAttribute("class","minphoto")
+        img.setAttribute("class","minphoto");
+        img.setAttribute("data-src",common.staticurl+data.photo[i].src)
         img.setAttribute("src",common.staticurl+data.photo[i].src_min)
         document.getElementById("gallery").appendChild(img);
-        document.getElementById("gallery").appendChild(img);
-        document.getElementById("gallery").appendChild(img);
-        document.getElementById("gallery").appendChild(img)
+        img.onclick=function(){
+            var show=document.getElementById("showphoto");
+            var showimg=document.getElementById("showimg");
+            showimg.src=this.dataset.src;
+            show.style.display='block';
+        }
+        
     }
+    var photo=document.getElementById("showphoto");
+    photo.onclick=function(){
+        this.style.display='none';
+    }
+    // for(var i=0;i<num;i++){
+    //     var img=document.createElement("img");
+    //     img.setAttribute("class","minphoto");
+    //     img.setAttribute("data-src",common.staticurl+data.photo[i].src)
+    //     img.setAttribute("src",common.staticurl+data.photo[i].src_min)
+    //     document.getElementById("gallery").appendChild(img);
+        
+        
+    // }for(var i=0;i<num;i++){
+    //     var img=document.createElement("img");
+    //     img.setAttribute("class","minphoto");
+    //     img.setAttribute("data-src",common.staticurl+data.photo[i].src)
+    //     img.setAttribute("src",common.staticurl+data.photo[i].src_min)
+    //     document.getElementById("gallery").appendChild(img);
+        
+        
+    // }
     
    }
 );
--- js/service.js
+++ js/service.js
@@ -1,10 +1,21 @@
 $(document).ready(function(){
     $(".container a").click(function(){
+        
+       if($(this).prev(".showdetail").hasClass("show")){
+        $(this).next(".showdetail").addClass("bordertop")
+       }
+       console.log($(this).next())
+       if($(this).next().next().next(".showdetail").hasClass("show")){
+        $(this).next().next().next(".showdetail").addClass("bordertop")
+       }
         $(this).hide();
-        $(this).next(".showdetail").show();
+        $(this).next(".showdetail").show().addClass("show");
     })
     $(".showdetail span").click(function(){
-        $(this).parent().hide();
+        if($(this).parent().next().next().hasClass("show")){
+            $(this).parent().next().next().removeClass("bordertop").removeClass("show")
+           }
+        $(this).parent().removeClass("bordertop").removeClass("show").hide();
         $(this).parent().prev("a").show();
     })
 })

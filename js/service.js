51e4234debbcbd8186dcf9f3031925bb16804e7e
Neil SHEN <uping_s@yahoo.com>
2018-09-12 23:19:42 +0800

    border solid

--- js/service.js
+++ js/service.js
@@ -3,21 +3,21 @@ $(document).ready(function(){
     $(".container a").hide();
     document.onscroll=function(){
         var scrollTop=document.body.scrollTop+document.documentElement.scrollTop;
-        console.log($(".container")[0].scrollTop,document.body.scrollTop,document.documentElement.scrollTop);
+        // console.log(!$(".first").next(".showdetail").hasClass("show"),document.documentElement.scrollTop);
         
-        if(scrollTop > 60&&scrollTop<260&&!isrun1) {
+        if(scrollTop > 60&&scrollTop<260&&!isrun1&&!$(".first").next(".showdetail").hasClass("show")) {
             isrun1=true;
            $(".first").show().addClass("animated fadeInUp1");
            
-        }else if(scrollTop >= 240&&scrollTop<430&&!isrun2) {
+        }else if(scrollTop >= 240&&scrollTop<430&&!isrun2&&!$(".second").next(".showdetail").hasClass("show")) {
             isrun2=true;
             $(".second").show().addClass("animated fadeInUp1");
-        }else if(scrollTop >= 420&&scrollTop<530&&!isrun3) {
+        }else if(scrollTop >= 380&&scrollTop<530&&!isrun3&&!$(".third").next(".showdetail").hasClass("show")) {
             isrun3=true;
             $(".third").show().addClass("animated fadeInUp1");
-        }else if(scrollTop>=500&&!isrun4) {
+        }else if(scrollTop>=450&&!isrun4&&!$(".last").next(".showdetail").hasClass("show")) {
             isrun4=true;
-            $(".container a").show().addClass("animated fadeInUp1");
+            $(".last").show().addClass("animated fadeInUp1");
             
         }
     }
@@ -29,7 +29,7 @@ $(document).ready(function(){
         $(this).next(".showdetail").addClass("bordertop")
         
        }
-       console.log($(this).next())
+    //    console.log($(this).next())
        if($(this).next().next().next(".showdetail").hasClass("show")){
         $(this).next().next().next(".showdetail").addClass("bordertop")
        }
@@ -37,6 +37,9 @@ $(document).ready(function(){
         $(this).next(".showdetail").show().addClass("show");
     })
     $(".showdetail span").click(function(){
+        console.log( $(this).parent(".showdetail").prev("a").prev(".showdetail").prev("a"))
+       $(this).parent(".showdetail").prev("a").prev(".showdetail").prev("a").length!=0?$(this).parent(".showdetail").prev("a").prev(".showdetail").prev("a").css("border-bottom","solid white 2.5px"):"";
+       $(this).parent(".showdetail").next("a").length!=0?$(this).parent(".showdetail").next("a").css("border-top","solid white 2.5px"):"";
         // $(this).parent().prev().prev().prev("a").css("border-bottom","2.5px");
         // $(this).parent().prev().next().next("a").css("border-top","2.5px");
         if($(this).parent().next().next().hasClass("show")){
